ebf539ff2c0776b7b2fc18be5f7bf3a345c4df36
NickHellemans <60689153+NickHellemans@users.noreply.github.com>
2023-06-26 21:45:59 +0200

    Update work section

--- dev_portfolio/src/assets/index.js
+++ dev_portfolio/src/assets/index.js
@@ -25,10 +25,13 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import ap from "./company/ap.png";
+import bebold from "./company/bebold.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+
 
 export {
   logo,
@@ -56,6 +59,8 @@ export {
   shopify,
   starbucks,
   tesla,
+  ap,
+  bebold,
   carrent,
   jobit,
   tripguide,
--- dev_portfolio/src/constants/index.js
+++ dev_portfolio/src/constants/index.js
@@ -23,6 +23,8 @@ import {
   jobit,
   tripguide,
   threejs,
+  ap,
+  bebold,
 } from "../assets";
 
 export const navLinks = [
@@ -116,55 +118,27 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    title: "Researcher intern",
+    company_name: "AP Hogeschool - Immersive lab",
+    icon: ap,
+    iconBg: "#ffff",
+    date: "Feb 2023 - May 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Research within the field of Novel View Synthesis",
+      "Capturing fotorealistic data to create real life virtual nature scene experiences in a cost efficient way",
+      "Researching an optimal camera rig using Neural Radiance Fields to capture data to create these virtual scenes"
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    title: "Software developer intern",
+    company_name: "Be Bold",
+    icon: bebold,
+    iconBg: "#ffff",
+    date: " Feb 2020 - May 2020",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+    "Developing a data model exporter web application from scratch with React, Node, Express & Firebase",
+    "Individual project to provide an end-to-end solution",
+    "Developing a dashboard to display important data in the office with React"
     ],
   },
 ];
